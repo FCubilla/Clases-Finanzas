8cc9a597c5bd39b45d740956de72691897b0a26c
FCubilla <facu_10.98@hotmail.com>
2026-08-03 14:05:10 -0300

    Fix GitHub Pages routing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = process.env.GITHUB_REPOSITORY?.split('/').pop() ?? 'facupadel'
+const repoName = 'Clases-Finanzas'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
